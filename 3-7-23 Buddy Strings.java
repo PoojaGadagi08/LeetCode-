c34cdf6ef718c9b340b9ff3a283f60a1fc7ae56e
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-03 06:01:30 +0530

    Create 3-7-23 Buddy Strings.java

---
+++ 3-7-23 Buddy Strings.java
@@ -0,0 +1,68 @@
+Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+
+For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+ 
+
+Example 1:
+
+Input: s = "ab", goal = "ba"
+Output: true
+Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+Example 2:
+
+Input: s = "ab", goal = "ab"
+Output: false
+Explanation: The only letters you can swap are s[0] = 'a' and s[1] = 'b', which results in "ba" != goal.
+Example 3:
+
+Input: s = "aa", goal = "aa"
+Output: true
+Explanation: You can swap s[0] = 'a' and s[1] = 'a' to get "aa", which is equal to goal.
+
+
+
+
+  // class Solution {
+//     public boolean buddyStrings(String s, String goal) {
+//         StringBuilder sb=new StringBuilder(s);
+//         sb.reverse();
+//         String a=sb.toString();
+//         if(a.equals(goal))
+//         return true;
+//         return false;
+//     }
+// }
+
+
+
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        int m = s.length(), n = goal.length();
+        if (m != n) {
+            return false;
+        }
+        int diff = 0;
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; ++i) {
+            int a = s.charAt(i), b = goal.charAt(i);
+            ++cnt1[a - 'a'];
+            ++cnt2[b - 'a'];
+            if (a != b) {
+                ++diff;
+            }
+        }
+        boolean f = false;
+        for (int i = 0; i < 26; ++i) {
+            if (cnt1[i] != cnt2[i]) {
+                return false;
+            }
+            if (cnt1[i] > 1) {
+                f = true;
+            }
+        }
+        return diff == 2 || (diff == 0 && f);
+    }
+}

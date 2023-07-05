38c7897a2a9d1516721c0527e1a2f86b797e209d
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-05 19:56:16 +0530

    Update 5-7-23 Longest Subarray of 1's After Deleting One Element.java

--- 5-7-23 Longest Subarray of 1's After Deleting One Element.java
+++ 5-7-23 Longest Subarray of 1's After Deleting One Element.java
@@ -23,23 +23,26 @@ Explanation: You must delete one element.
 
 
 
-  class Solution {
+ class Solution {
     public int longestSubarray(int[] nums) {
-        int num_of_zeros_allowed = 1;
-        int left_ptr = 0;
-        int right_ptr = 0;
+        int n = nums.length;
 
-        for (right_ptr = 0; right_ptr < nums.length; right_ptr++) {
-            boolean is_zero = nums[right_ptr] == 0;
-            num_of_zeros_allowed -= is_zero ? 1 : 0;
+        int left = 0;
+        int zeros = 0;
+        int ans = 0;
 
-            if (num_of_zeros_allowed < 0) {
-                boolean is_zero_left = nums[left_ptr] == 0;
-                num_of_zeros_allowed += is_zero_left ? 1 : 0;
-                left_ptr += 1;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0) {
+                zeros++;
             }
+            while (zeros > 1) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1 - zeros);
         }
-
-        return right_ptr - left_ptr;
+        return (ans == n) ? ans - 1 : ans;
     }
 }

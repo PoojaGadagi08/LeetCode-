2792fd09455a9d87b2db6bc59d43d1c517d3df09
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-08 13:08:10 +0530

    Update 8-7-23  Put Marbles in Bags.java

--- 8-7-23  Put Marbles in Bags.java
+++ 8-7-23  Put Marbles in Bags.java
@@ -46,3 +46,37 @@ Since both the maximal and minimal score are the same, we return 0.
         return maxScore - minScore;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution
+// {
+//     // arr[]: input array
+//     // n: size of the array
+//     //Function to find triplets with zero sum.
+// 	public boolean findTriplets(int arr[] , int n)
+//     {
+//         //add code here.
+//         int c=0;
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=i+1;j<arr.length;j++){
+//                 for(int k=j+1;k<arr.length;k++){
+//                 if(arr[i]+arr[j]+arr[k]==0)
+//                 c++;
+//             }
+//         }
+//         }
+//         if(c>1) return true;
+//         return false;
+//     }
+// }

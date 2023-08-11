8db9264cf80d160cb2bc695d8c691fe2dadb6649
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-08-11 11:47:57 +0530

    Create 11-8-23 coin change 2 .java

---
+++ 11-8-23 coin change 2 .java
@@ -0,0 +1,69 @@
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+You may assume that you have an infinite number of each kind of coin.
+
+The answer is guaranteed to fit into a signed 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+Example 2:
+
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
+Example 3:
+
+Input: amount = 10, coins = [10]
+Output: 1
+ 
+
+Constraints:
+
+1 <= coins.length <= 300
+1 <= coins[i] <= 5000
+All the values of coins are unique.
+0 <= amount <= 5000
+
+
+
+
+  class Solution {
+    public int change(int amount, int[] coins) {
+        Map<String, Integer> cache = new HashMap<>();
+        return change(amount, 0, cache, coins);
+    }
+    
+    public int change(int amount, int start, Map<String, Integer> cache, int[] coins) {
+        if (amount < 0) {
+            return 0;
+        }
+        
+        if (amount == 0) {
+            return 1;
+        }
+        
+        String key = amount + "," + start;
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        
+        int count = 0;
+        for (int i = start; i < coins.length; ++ i) {
+            count += change(amount - coins[i], i, cache, coins);
+        }
+        
+        cache.put(key, count);
+        return count;
+    }
+}

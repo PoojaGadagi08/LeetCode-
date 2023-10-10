904e80edf8b645b582481d7f841702c3c3bac05d
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-10-10 15:59:22 +0530

    Create best-time-to-buy-and-sell-stock-ii.java

---
+++ best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,23 @@
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+
+
+
+  class Solution {
+    public int maxProfit(int[] prices) {
+    int profit = 0, i = 0;
+    while (i < prices.length) {
+        // find next local minimum
+        while (i < prices.length-1 && prices[i+1] <= prices[i]) i++;
+        int min = prices[i++]; // need increment to avoid infinite loop for "[1]"
+        // find next local maximum
+        while (i < prices.length-1 && prices[i+1] >= prices[i]) i++;
+        profit += i < prices.length ? prices[i++] - min : 0;
+    }
+    return profit;
+}
+}

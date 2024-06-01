e76e4279bd78d69895c766fe7bb2bf3e2864d11f
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-06-01 08:19:15 +0530

    Create 1-6-24 score-of-a-string.java

---
+++ 1-6-24 score-of-a-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int scoreOfString(String s) {
+         int score = 0;
+        
+        // Iterate over the characters of the string starting from the second character
+        for (int i = 1; i < s.length(); i++) {
+            // Calculate the absolute difference between the ASCII values of adjacent characters
+            int diff = Math.abs(s.charAt(i) - s.charAt(i - 1));
+            // Add the difference to the score
+            score += diff;
+        }
+        
+        return score;
+    }
+}

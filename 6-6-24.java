aaffb5abbd872d4e098a6fbcbe77e8b89738c354
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-06-06 14:57:53 +0530

    Create 6-6-24.java

---
+++ 6-6-24.java
@@ -0,0 +1,13 @@
+    public boolean isNStraightHand(int[] hand, int W) {
+        Map<Integer, Integer> c = new TreeMap<>();
+        for (int i : hand) c.put(i, c.getOrDefault(i, 0)+1);
+        Queue<Integer> start = new LinkedList<>();
+        int last_checked = -1, opened = 0;
+        for (int i : c.keySet()) {
+            if (opened > 0 && i > last_checked + 1 || opened > c.get(i)) return false;
+            start.add(c.get(i) - opened);
+            last_checked = i; opened = c.get(i);
+            if (start.size() == W) opened -= start.remove();
+        }
+        return opened == 0;
+    }

ff30b606cacb9b4345737386b66c21bc3230c3b7
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-06-10 08:39:37 +0530

    Create 10-6-24.java

---
+++ 10-6-24.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n=heights.length;
+        int expected[]= Arrays.copyOf(heights, n);
+        Arrays.sort(expected);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(heights[i]!=expected[i]) cnt++;
+        }
+        return cnt;
+    }
+}
+
